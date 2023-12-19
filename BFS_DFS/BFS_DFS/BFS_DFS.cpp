f0faf1d43bbf2e9640c0507c2fc6de8298e14ca8
Alex Nekryach <a.nekryach@g.nsu.ru>
2023-12-20 01:44:41 +0700

    template for tests

---
+++ BFS_DFS/BFS_DFS/BFS_DFS.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "Graph.h"
+#include "BFS.h"
+#include "DFS.h"
+int main()
+{
+	Graph testGraph;
+	Vertex testVertex1("1");
+	Vertex testVertex2("2");
+	Vertex testVertex3("3");
+	Vertex testVertex4("4");
+	Vertex testVertex5("5");
+	Edge testEdge12(testVertex1, testVertex2);
+	Edge testEdge13(testVertex1, testVertex3);
+	Edge testEdge25(testVertex2, testVertex5);
+	Edge testEdge34(testVertex3, testVertex4);
+
+	testGraph.AddEdge(testEdge12);
+	testGraph.AddEdge(testEdge13);
+	testGraph.AddEdge(testEdge25);
+	testGraph.AddEdge(testEdge34);
+
+	BFSalgorithm BFS(testGraph, testVertex1);
+	BFS.initAlgorithm();
+	DFSalgorithm DFS(testGraph, testVertex1);
+	DFS.initAlgorithm();
+	return 0;
+}
+
+
+
